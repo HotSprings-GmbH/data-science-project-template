7a98c289f2afe13a179b14515682878a0603d46a
Millet, Philipp <philipp.millet@umlaut.com>
2022-08-30 16:38:17 +0200

    fix: specify mamba path to work in nested environments

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -1,4 +1,11 @@
+import os
+import pathlib
 import subprocess
+
+path_conda_executable = pathlib.Path(os.environ["CONDA_EXE"])
+path_mamba_executable = path_conda_executable.parent.joinpath(
+    path_conda_executable.name.replace("conda", "mamba")
+)
 
 # initialize new project repository
 subprocess.run(["git", "init"], check=True)
@@ -10,8 +17,18 @@ subprocess.run(["git", "branch", "-m", "main"], check=True)
 subprocess.run(["git", "remote", "add", "origin", "{{cookiecutter.repo_url}}"], check=True)
 
 # setup environment
-subprocess.run(["mamba", "env", "create", "-f", "environment.yaml", "--quiet"], check=True)
 subprocess.run(
-    ["mamba", "run", "--no-banner", "-n", "{{cookiecutter.env_name}}", "pre-commit", "install"],
+    [path_mamba_executable, "env", "create", "-f", "environment.yaml", "--quiet"], check=True
+)
+subprocess.run(
+    [
+        path_mamba_executable,
+        "run",
+        "--no-banner",
+        "-n",
+        "{{cookiecutter.env_name}}",
+        "pre-commit",
+        "install",
+    ],
     check=True,
 )
